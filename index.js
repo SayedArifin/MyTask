97342c5f54e885a448e22bf9dce2f5979ea0753c
Md Sayed Arifin <zinnatsaverdhaka@gmail.com>
2024-04-24 17:10:30 +0600

    24/04/24

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-document.
+let headlines = document.querySelectorAll(".headline");
+headlines.forEach(function (headline) {
+  headline.addEventListener("mouseover", function () {
+    headline.style.animationPlayState = "paused";
+  });
+  headline.addEventListener("mouseout", function () {
+    headline.style.animationPlayState = "running";
+  });
+});
